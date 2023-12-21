d11ad070a53cdfa5a9ae0ad146009f64201f9e6b
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-12-21 12:22:30 +0100

    create navbar with title and search

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className='flex justify-between py-3 px-6 border-b bg-black space-x-6'>
+        <h1 className='w-auto'>Game Quest</h1>
+        <form action='' className='w-full max-w-md'>
+          <div className='relative flex items-center text-gray-400 focus-within:text-gray-600'>
+            <MagnifyingGlassIcon className='w-5 h-5 absolute ml-3 pointer-events-none' />
+            <input
+              type='text'
+              name='search'
+              placeholder='Search Games'
+              autoComplete='off'
+              aria-label='search games'
+              className='w-full pr-3 pl-10 py-2 font-semibold placeholder-gray-500 text-black rounded-2xl border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2'
+            />
+          </div>
+        </form>
+      </div>
+    </nav>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default async function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Hello world</h1>
+      <h1>Game Quest</h1>
     </main>
   );
 }
