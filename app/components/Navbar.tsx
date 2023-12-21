d950b4c1a3311b5bfa2459339981b44fa005ecc6
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-12-21 12:39:22 +0100

    create light or dark mode switch

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import { ThemeSwitcher } from './ThemeSwitcher';
 
 export default function Navbar() {
   return (
     <nav>
-      <div className='flex justify-between py-3 px-6 border-b bg-black space-x-6'>
+      <div className='flex justify-between py-3 px-6 border-b bg-black '>
         <h1 className='w-auto'>Game Quest</h1>
         <form action='' className='w-full max-w-md'>
           <div className='relative flex items-center text-gray-400 focus-within:text-gray-600'>
@@ -18,6 +19,9 @@ export default function Navbar() {
             />
           </div>
         </form>
+        <div>
+          <ThemeSwitcher />
+        </div>
       </div>
     </nav>
   );
