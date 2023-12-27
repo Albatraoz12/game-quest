4fee9cf7ce7c8bf62a483545830450eb4ac72be0
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2023-12-27 13:46:30 +0100

    import card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import Card from './components/Card';
 import { fetchGames } from './lib/games';
 
 export default async function Home() {
   // const games = await fetchGames();
   // console.log(games);
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex flex-col items-center justify-between p-24'>
       <h1>Game Quest</h1>
+      <Card />
     </main>
   );
 }
