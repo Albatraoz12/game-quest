f27e9b2d42b53273d67b727a8b7193a37195ae65
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2023-12-27 07:12:33 +0100

    add code to fetch games

---
+++ app/lib/games.ts
@@ -0,0 +1,7 @@
+export const fetchGames = async () => {
+  const response = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page_size=1`
+  );
+  const data = await response.json();
+  return data;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { fetchGames } from './lib/games';
+
 export default async function Home() {
+  // const games = await fetchGames();
+  // console.log(games);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Game Quest</h1>
