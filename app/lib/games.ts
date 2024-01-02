cf51c3ddc15e4efd588da579296559736f6af786
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-01-02 13:54:18 +0100

    create function to loop through icons

--- app/lib/games.ts
+++ app/lib/games.ts
@@ -5,3 +5,20 @@ export const fetchGames = async () => {
   const data = await response.json();
   return data;
 };
+
+const mapPlatformsToIcons = (platforms: string[]): string[] => {
+  const platformIconMap: Record<string, string> = {
+    PC: 'BsWindows',
+    PlayStation: 'BsPlaystation',
+    Xbox: 'BsXbox',
+    'Apple Macintosh': 'BsApple',
+    Nintendo: 'BsNintendoSwitch',
+  };
+
+  const icons: string[] = platforms.map((platform: string) => {
+    const PlatformIcon: string | undefined = platformIconMap[platform];
+    return PlatformIcon || ''; // Handle unknown platforms by returning an empty string
+  });
+
+  return icons.filter((icon: string) => icon !== ''); // Remove empty strings (unknown platforms)
+};
