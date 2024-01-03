594cb94335022c89a9da8ad88eaacd82e36d05f5
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-01-03 13:59:23 +0100

    fetch more games

--- app/lib/games.ts
+++ app/lib/games.ts
@@ -1,6 +1,6 @@
 export const fetchGames = async () => {
   const response = await fetch(
-    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page_size=2`
+    `https://api.rawg.io/api/games?key=${process.env.API_KEY}`
   );
   const data = await response.json();
   return data;
