c98ff2715ac0bf346ed3f5518b494495fb6e0bf8
Dimosthenis Emmanouil <dimosthenis.emmanouil@chasacademy.com>
2024-01-03 12:56:27 +0100

    remove unused code

--- app/lib/games.ts
+++ app/lib/games.ts
@@ -1,24 +1,7 @@
 export const fetchGames = async () => {
   const response = await fetch(
-    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page_size=1`
+    `https://api.rawg.io/api/games?key=${process.env.API_KEY}&page_size=2`
   );
   const data = await response.json();
   return data;
-};
-
-const mapPlatformsToIcons = (platforms: string[]): string[] => {
-  const platformIconMap: Record<string, string> = {
-    PC: 'BsWindows',
-    PlayStation: 'BsPlaystation',
-    Xbox: 'BsXbox',
-    'Apple Macintosh': 'BsApple',
-    Nintendo: 'BsNintendoSwitch',
-  };
-
-  const icons: string[] = platforms.map((platform: string) => {
-    const PlatformIcon: string | undefined = platformIconMap[platform];
-    return PlatformIcon || ''; // Handle unknown platforms by returning an empty string
-  });
-
-  return icons.filter((icon: string) => icon !== ''); // Remove empty strings (unknown platforms)
 };
